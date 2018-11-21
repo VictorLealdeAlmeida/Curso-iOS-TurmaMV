1c748985db0b0bb3474902ba4c08f8aecb1cc1f0
Hilton Pintor <hiltonpintor@gmail.com>
2018-11-21 13:59:10 -0300

    Aula 01 - Exemplo de outlets UIKit

---
+++ Aula 01/OutletsMV/OutletsMV/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Aula1MV
+//
+//  Created by Hilton Pintor Bezerra Leite on 13/11/18.
+//  Copyright © 2018 Hilton Pintor Bezerra Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var imagem: UIImageView!
+    @IBOutlet weak var caixaTexto: UITextField!
+    @IBOutlet weak var botao: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func apertouBotao(_ sender: UIButton) {
+        
+        
+    }
+    
+}
+
