560fb137a932dee716cc4e4a98735e059982707f
Hilton Pintor <hiltonpintor@gmail.com>
2018-11-23 10:04:42 -0300

    Aula 04 - Projeto download de imagem

---
+++ Aula 04/RequestImagemMV/RequestImagemMV/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  RequestImagemMV
+//
+//  Created by Hilton Pintor Bezerra Leite on 23/11/18.
+//  Copyright © 2018 Hilton Pintor Bezerra Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBAction func tapDownloadImageButton(_ sender: Any) {
+        let imageURL = URL(string: "https://media.lovemondays.com.br/logos/fdbb55/mv-original.png")!
+        
+        let downloadTask = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            if let data = data {
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage(data: data)
+                }
+            }
+        }
+        
+        downloadTask.resume()
+    }
+    
+}
+
